c13fcc2425bf77e7344ee676c4a57fe6d0cfc178
0xBahamoot <lam@autonomous.nyc>
2020-11-18 09:59:46 +0700

    .

--- sidebars.js
+++ sidebars.js
@@ -2,6 +2,6 @@ module.exports = {
   someSidebar: {
     'Getting Started':['doc1','vs'],
     Tutorials:['tutorial/tut-pde','tutorial/tut-stake', 'tutorial/tut-tx', 'tutorial/tut-remote'],
-    APIs: [{'RPC':['api/rpc/staking','api/rpc/pde','api/rpc/chainstate','api/rpc/transaction','api/rpc/account']},{'Sim':['api/sim/block','api/sim/account']}, 'api/event','api/other'],
+    APIs: [{'RPC':['api/rpc/staking','api/rpc/pde','api/rpc/chainstate','api/rpc/transaction','api/rpc/account-rpc']},{'Sim':['api/sim/block','api/sim/account-sim']}, 'api/event','api/other'],
   },
 };
