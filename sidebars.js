db2636a29b971af9609d9e8caa8e1b4e09ff2cf4
0xBahamoot <lam@autonomous.nyc>
2020-12-08 15:04:30 +0700

    .

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,7 @@
 module.exports = {
   someSidebar: {
     'Getting Started':['doc1','vs'],
-    Tutorials:['tutorial/tut-pde','tutorial/tut-stake', 'tutorial/tut-tx', 'tutorial/tut-remote'],
+    Tutorials:['tutorial/tut-pde','tutorial/tut-stake', 'tutorial/tut-tx', 'tutorial/tut-remote', 'tutorial/tut-lightnode'],
     APIs: [{'RPC':['api/rpc/validator','api/rpc/pde','api/rpc/chainstate','api/rpc/transaction','api/rpc/account-rpc', 'api/rpc/block-rpc']},{'Sim':['api/sim/block','api/sim/account-sim']}, 'api/event','api/other'],
   },
 };
